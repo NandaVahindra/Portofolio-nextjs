f29d550554c2e6e6eba65283f564cdf36b842c4c
NandaVahindra <nandavahindra@gmail.com>
2024-07-05 11:07:07 +0700

    change url for handspeak project

--- data/index.ts
+++ data/index.ts
@@ -75,7 +75,7 @@ export const navItems = [
       des: "An application designed to detect and interpret sign language through real-time hand gesture recognition.",
       img: "/hand.png",
       iconLists: ["/kotlinlang-icon.svg", "/nodejs-icon.svg", "/google-cloud-svgrepo-com.svg"],
-      link: "https://github.com/NandaVahindra/HandSpeak-Backend",
+      link: "https://handspeak-page-5ffg2znloq-et.a.run.app/",
     },
     {
       id: 2,
